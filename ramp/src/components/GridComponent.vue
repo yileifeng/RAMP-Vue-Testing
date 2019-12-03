25238ff8115d4b4ba88c13eb58a18d97e7ee50af
Ryan Coulson <ryan.coulson@canada.ca>
2019-12-03 13:45:49 -0500

    change grid theme

--- ramp/src/components/GridComponent.vue
+++ ramp/src/components/GridComponent.vue
@@ -2,7 +2,7 @@
 	<div>
 		<ag-grid-vue
 			style="width: 500px; height: 300px;"
-			class="ag-grid-test ag-theme-balham"
+			class="ag-grid-test ag-theme-material"
 			:gridOptions="gridOptions"
 			:columnDefs="columnDefs"
 			:rowData="rowData"
@@ -16,7 +16,7 @@
 
 <script>
 import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
-import '@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css';
+import '@ag-grid-community/all-modules/dist/styles/ag-theme-material.css';
 import { AgGridVue } from '@ag-grid-community/vue';
 import { AllCommunityModules } from '@ag-grid-community/all-modules';
 import { TableBuilder } from '../enhancedTable/index';
@@ -48,16 +48,34 @@ export default {
 				field: 'COUNTRY',
 				sortable: true,
 				filter: 'agTextColumnFilter',
-				lockPosition: true
+				lockPosition: true,
+				width: 500
 			},
-			{ headerName: 'NAME', field: 'NAME', sortable: true, filter: 'agTextColumnFilter', lockPosition: true },
-			{ headerName: 'DATE', field: 'DATE', sortable: true, filter: 'agDateColumnFilter', lockPosition: true }
+			{
+				headerName: 'NAME',
+				field: 'NAME',
+				sortable: true,
+				filter: 'agTextColumnFilter',
+				lockPosition: true,
+				width: 500
+			},
+			{
+				headerName: 'DATE',
+				field: 'DATE',
+				sortable: true,
+				filter: 'agDateColumnFilter',
+				lockPosition: true,
+				width: 300
+			}
 		];
 
 		this.rowData = [
 			{ OBJECTID: 1, COUNTRY: 'Mexico', NAME: 'Cornwall Pipeline', DATE: '01/01/2020' },
 			{ OBJECTID: 2, COUNTRY: 'Canada', NAME: 'Mainline', DATE: '12/25/2019' },
-			{ OBJECTID: 3, COUNTRY: 'United States', NAME: 'Bluewater Pipeline Co', DATE: '11/29/2019' }
+			{ OBJECTID: 3, COUNTRY: 'United States', NAME: 'Bluewater Pipeline Co', DATE: '11/29/2019' },
+			{ OBJECTID: 4, COUNTRY: 'United States', NAME: 'Bluewater Pipeline Co', DATE: '11/29/2019' },
+			{ OBJECTID: 5, COUNTRY: 'Canada', NAME: 'Bluewater Pipeline Co', DATE: '11/29/2019' },
+			{ OBJECTID: 6, COUNTRY: 'United States', NAME: 'Bluewater Pipeline Co', DATE: '11/29/2019' }
 		];
 
 		this.frameworkComponents = { agColumnHeader: CustomHeader };
@@ -79,8 +97,12 @@ export default {
 <style>
 .ag-grid-test {
 	position: absolute;
-	margin-left: 500px;
-	width: 602px !important;
+	margin-left: 375px;
+	width: calc(100% - 22vw) !important;
+	height: 50vh !important;
 	bottom: 500px;
+	box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+		0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+	top: 0px;
 }
 </style>
